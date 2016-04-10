19c8bf9ce6a5a3fd4f8fe9c4bb16766270349b32
Chilipp <p.sommer@posteo.de>
2016-04-10 14:37:28 +0200

    Updated to new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='psyplot',
-      version='0.2.0',
+      version='0.2.1',
       description='Python package for interactive data visualization',
       long_description=readme(),
       classifiers=[
