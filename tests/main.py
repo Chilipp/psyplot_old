f4238fbcab0dcece80cf08e96e96f0ee996f1bd1
Chilipp <p.sommer@posteo.de>
2016-04-20 08:40:29 +0200

    Made tests verbose and failfast

--- tests/main.py
+++ tests/main.py
@@ -4,7 +4,7 @@
 This script may be used from the command line run all tests from the psyplot
 package. See::
 
-    python create_references.py -h
+    python main.py -h
 
 for details. You can also use the py.test module (which however leads to a
 failure of some tests in python 3.4)
@@ -15,10 +15,9 @@ import os
 
 import unittest
 
-test_suite = unittest.defaultTestLoader.discover(
-    os.path.join(bt.test_dir, 'tests/'))
+test_suite = unittest.defaultTestLoader.discover(bt.test_dir)
 
 
 if __name__ == '__main__':
-    test_runner = unittest.TextTestRunner()
+    test_runner = unittest.TextTestRunner(verbosity=2, failfast=True)
     test_runner.run(test_suite)
