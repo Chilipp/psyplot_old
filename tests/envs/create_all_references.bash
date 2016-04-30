e51cdc14d523e44acd1ea5092a18e7769175bee9
Chilipp <p.sommer@posteo.de>
2016-04-30 21:26:58 +0200

    Changed reference creation to update numpy before psyplot

--- tests/envs/create_all_references.bash
+++ tests/envs/create_all_references.bash
@@ -13,12 +13,16 @@ for f in `ls environment_*.yml`; do
 	python_bin=${envs_dir}/${env}/bin/python
 	pip_bin=${envs_dir}/${env}/bin/pip
 	conda_bin=${envs_dir}/${env}/bin/conda
-	# check if psyplot already is installed
-	${python_bin} -c "import psyplot" > /dev/null
-	if [[ $? == 1 ]]; then ${conda_bin} install -c chilipp psyplot; fi
 	# try to import numpy and if it doesn't work (due to cartopy), update to current version
 	${python_bin} -c "import numpy" > /dev/null
 	if [[ $? == 1 ]]; then ${pip_bin} install --upgrade numpy; fi
+	# check if psyplot already is installed
+	${python_bin} -c "import psyplot" > /dev/null
+	if [[ $? == 1 ]]; then 
+	    source activate ${env}
+	    conda install -c chilipp psyplot
+	    source deactivate
+    fi
 	# create reference figures
 	${python_bin} ../create_references.py &> ref_${env}.log && conda env remove -y -n ${env} || echo "Error occured when creating references for ${env}!" &
 	
