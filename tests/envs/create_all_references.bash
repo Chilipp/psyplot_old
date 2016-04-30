f104eafaa4d3e3d865b34b202ee07aac6059cf40
Chilipp <p.sommer@posteo.de>
2016-04-30 19:28:57 +0200

    Removed activation of the created conda environment

--- tests/envs/create_all_references.bash
+++ tests/envs/create_all_references.bash
@@ -4,16 +4,18 @@
 # environment. The conda environment name is expected to be psyplot_${key} with the
 # key of the corresponding file. 
 # The results can be seen in ref_psyplot_${key}.log
+envs_dir=`conda info --root`/envs
 for f in `ls environment_*.yml`; do
 
 	conda env create -f ${f}
 	env=${f%.yml}
 	env=psyplot_${env##environment_}
-	source activate ${env}
+	python_bin=${envs_dir}/${env}/bin/python
+	conda_bin=${envs_dir}/${env}/bin/conda
 	# check if psyplot already is installed
-	python -c "import psyplot" > /dev/null
-	if [[ $? == 1 ]]; then conda install -c chilipp psyplot; fi
+	${python_bin} -c "import psyplot" > /dev/null
+	if [[ $? == 1 ]]; then ${conda_bin} install -c chilipp psyplot; fi
 	# create reference figures
-	python ../create_references.py &> ref_${env}.log || echo "Error occured when creating references for ${env}!" && source deactivate && conda env remove -y -n ${env} &
+	${python_bin} ../create_references.py &> ref_${env}.log || echo "Error occured when creating references for ${env}!" && conda env remove -y -n ${env} &
 	
 done
