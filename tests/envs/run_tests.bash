b1d5d83d2b74fdf32b540d7096a0bb8e73a58a86
Chilipp <p.sommer@posteo.de>
2016-09-13 15:06:49 +0200

    Install cartopy manually for matplotlib 1.5

--- tests/envs/run_tests.bash
+++ tests/envs/run_tests.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Run the tests for all environment files in this folders. Environment files are
-# expected to be like "environment_${key}.yml", where ${key} is the unique key for the 
+# expected to be like "environment_${key}.yml", where ${key} is the unique key for the
 # environment. The conda environment name is expected to be psyplot_${key} with the
-# key of the corresponding file. 
+# key of the corresponding file.
 # The results can be seen in test_psyplot_${key}.log
+set -x
 envs_dir=`conda info --root`/envs
 WORK=`pwd`
 for f; do
@@ -16,17 +17,21 @@ for f; do
     pip_bin=${envs_dir}/${env}/bin/pip
     conda_bin=${envs_dir}/${env}/bin/conda
     # try to import numpy and if it doesn't work (due to cartopy), update to current version
+    source activate ${env}
+    ${conda_bin} install -y -c scitools cartopy
+    source deactivate
     ${python_bin} -c "import numpy" > /dev/null
     if [[ $? == 1 ]]; then ${pip_bin} install --upgrade numpy; fi
     # check if psyplot already is installed
     ${python_bin} -c "import psyplot" > /dev/null
-    if [[ $? == 1 ]]; then 
+    if [[ $? == 1 ]]; then
         source activate ${env}
         conda install --no-deps -c chilipp psyplot
         source deactivate
     fi
     # run tests
-    mkdir ${env} && cd ${env}
+    mkdir ${env}
+    cd ${env}
     touch matplotlibrc
     ${pytest_bin} --html=../${env}.html --cov=psyplot --cov-report html ../../ &> ../test_${env}.log && mv htmlcov $WORK/cov_${env} && cd $WORK && rm -r ${env} && conda env remove -y -n ${env} || echo "Error occured when testing for ${env}!"
 done
